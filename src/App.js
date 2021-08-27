0b7a95b1df69e79a245f7b074bea02fc66db20d3
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2021-08-27 14:20:39 +0100

    tESTING CI/DI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Amplify
+          Hello Amplify <span>React App</span>
         </p>
         <a
           className="App-link"
